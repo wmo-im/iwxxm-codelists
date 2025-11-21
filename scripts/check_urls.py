a16409e984fc4cc2fdd10988ca243ed445ff3928
amilan17 <amilan@wmo.int>
2025-11-21 09:00:23 +0100

    remove test for observable property and observation type in check_urls script

--- scripts/check_urls.py
+++ scripts/check_urls.py
@@ -95,7 +95,8 @@ for f in glob.glob('**/*.ttl', recursive=True):
 
     # Skip AerodromePresentOrForecastWeather and AerodromeRecentWeather as they are pointers to http://codes.wmo.int/306/4678
     ftest = os.path.basename(os.path.dirname(f))
-    if ftest == 'AerodromePresentOrForecastWeather' or ftest == 'AerodromeRecentWeather':
+    if ftest == 'AerodromePresentOrForecastWeather' or ftest == 'AerodromeRecentWeather' or ftest == 'observable-property' or ftest == 'observation-type':
+
         continue
 
     relf = f.replace('.ttl', '')[len('TTL/'):]
